3b94914ae7ea993a6fae28fb968be19d671d2a1d
Eric Leung <mirageknight213@gmail.com>
2013-12-27 13:11:45 -0600

    update README and triangle script

--- highDivisibleTriangleNum.pl
+++ highDivisibleTriangleNum.pl
@@ -2,31 +2,32 @@
 
 # Program 'highDivisibleTriangleNum'
 # What is the value of the first triangle number to have over five hundred
-# divisors
+# divisors?
 
 # Farthest: 9033375
 
 use strict;
 
-my $sors = 0;
-my $num = 0;
-my $triangle;
+my $sors = 0; # number of divisors
+my $num = 0; # triangle number place
+my $triangle; # triangle numnber
 
-my $test1 = &sum(10);
+# testing phase
+my $test1 = &sum(10); # find 
 print "$test1\n";
 my @test = &divisors($test1);
 print "@test\n";
 print scalar(@test),"\n";
 
-until ( $sors > 500 ) {
-    $num++;
+until ( $sors > 500 ) { # until num of divisors greater than 500
+    $num++; # increase number
 #   print "Num: $num\n";
-    my $tri = &sum($num);
+    my $tri = &sum($num); 
     $triangle = $tri;
 #   print "Tri Num: $tri\n";
     my @divide = &divisors($tri);
     $sors = scalar( @divide ); 
-#   print "Num Divisors: $sors\n\n";
+#   print "$sors\n\n";
 
     if ( $num % 250 == 0 ) {
         print "Triangle Num: $tri\n";
@@ -35,17 +36,19 @@ until ( $sors > 500 ) {
 
 }
 
+# return triangle number
 print "$triangle\n";
 
-sub sum { # sum 
-    my ($n) = @_;
-    my $sum = $n * ($n+1) / 2;
-    return $sum;
+# subroutines for script
+sub sum { # sum of first 'n' numbers
+    my ($n) = @_; # take number
+    my $sum = $n * ($n+1) / 2; # Gauss' formula
+    return $sum; # return result
 }
 
 sub divisors { # Find divisors for number
-    my ($n) = @_;
-    my @divide;
+    my ($n) = @_; # take in number
+    my @divide; # make divisors array
     for ( my $k = 1; $k < $n/2; $k++ ) {
         my $l = $n / $k;
         $l = int($l);
