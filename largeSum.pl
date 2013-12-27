f360ec8924f34ec9487734ae3712c313243297b0
Eric Leung <mirageknight213@gmail.com>
2013-12-26 20:10:37 -0600

    large sum problem work flow draft

---
+++ largeSum.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# Program 'largeSum'
+# Work out the first ten digits of the sum of the following one-
+# hundred 50-digit numbers
+
+# initialize all numbers into array
+
+# interate over all of them mod 10,000,000,000 (10 digits)
+
+# add mod 10,000,000,000
+
+# return answer
