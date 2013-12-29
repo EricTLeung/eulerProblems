de10e3aa8f5727e245491aab23577aafad934c2f
Eric Leung <mirageknight213@gmail.com>
2013-12-28 19:09:22 -0600

    Names Scrores problem file and draft script

---
+++ namesScores.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+
+# Program 'namesScores'
+
+# open file with names
+
+# get names into array
+
+# order names alphabetically
+
+# create hash of letter with position
+
+# for each name, calculate alphabet value
+
+# multiply it by position
+
+# add to total 
+
+# return total of all names
