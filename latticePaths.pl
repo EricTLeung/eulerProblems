2742de4f65198a94f83ad72948f5ba942c1bb112
Eric Leung <mirageknight213@gmail.com>
2014-01-12 18:58:21 -0600

    add more of algorithm

--- latticePaths.pl
+++ latticePaths.pl
@@ -6,7 +6,11 @@ use warnings;
 # Program 'latticePaths.pl'
 
 my $n = 20; # stands for 20x20
+my $moves = 2 * $n; # num of total moves
 
-my $total = 2**$n;
+my $num = 2**$n; # find number of routes to center diagonal
+$num -= 2; # take two away ...
+
+my $total = 2; # ... and add it to the total
 
 
