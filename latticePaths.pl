a9fb9740509d39b3cbe1a27954a95e65b724fdcf
etleung21 <mirageknight213@gmail.com>
2014-06-01 02:40:25 -0500

    update latticePaths.pl
    found solution, need to code it up, use combinations of going down and
    right to solve the problem

--- latticePaths.pl
+++ latticePaths.pl
@@ -4,13 +4,19 @@ use strict;
 use warnings;
 
 # Program 'latticePaths.pl'
+# Starting in the top left corner of a 2x2 grid, and only being able to move to
+# the right and down, there are exactly 6 routes to the bottom right corner.
+# How many such routes are there through 20x20 grid?
 
 my $n = 20; # stands for 20x20
-my $moves = 2 * $n; # num of total moves
+my $totalLength = 2 * $n; # total length of any path
 
-my $num = 2**$n; # find number of routes to center diagonal
-$num -= 2; # take two away ...
+my $top = &fact($totalLength); # get top of combination formula
+print "$top\n";
 
-my $total = 2; # ... and add it to the total
+my $bottom = &fact($n); # part of bottom of combination formula
+print "$bottom\n";
 
+my $total = &fact($totalLength) / ( 2 * &fact($n) );
 
+print "$total\n";
