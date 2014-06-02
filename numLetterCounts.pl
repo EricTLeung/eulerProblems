163c9207e6e9da57a3ea530c6be9ce2552440ce7
etleung21 <mirageknight213@gmail.com>
2014-06-02 01:33:46 -0500

    create numLetterCounts
    initial draft of script

---
+++ numLetterCounts.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+# Program 'numLettersCounts.pl'
+# If the numbers 1 to 4 are written out in words: one, twi, three, four, five,
+# then there are 3+3+5+4+4 = 19 letters used in total. If all the numbers from 1
+# to 1000 (one thousand) inclusive were written out in words, how many letters
+# would be used?
+
+# write out hash for all numbers
+
+# generate all numbers
+
+# iterate through numbers and check hash
+
+# check one digit numbers
+
+# save sum of all one digit numbers
+
+# check two digit numbers
+
+# save sum of all 2 digit numbers
+
+# check three digit numbers
+
+# add eleven to total for 'one thousand'
+
+# print answer
