12f54f2fd023f7e230c003406212160e49d20cea
etleung21 <mirageknight213@gmail.com>
2014-06-06 17:40:27 -0500

    finish one and two digit numbers
    finish two loops in script

--- numLetterCounts.pl
+++ numLetterCounts.pl
@@ -7,61 +7,96 @@
 # would be used?
 
 # write out hash for all numbers
-my %hash = (
+my %oneHash = (
   #num => length of word number
-  1    => 3, # one = 3
-  2    => 3, # two = 3
-  3    => 5, # three = 5
-  4    => 4, # four = 4
-  5    => 4, # five = 4
-  6    => 3, # six = 3
-  7    => 5, # seven = 5
-  8    => 5, # eight = 5
-  9    => 4, # nine = 4
-  10   => 3, # ten = 3
-  11   => 6, # eleven = 6
-  12   => 6, # twelve = 6
-  13   => 8, # thirteen = 8
-  14   => 8, # fourteen = 8
-  15   => 7, # fifteen = 7
-  16   => 7, # sixteen = 7
-  17   => 9, # seventeen = 9
-  18   => 8, # eighteen = 8
-  19   => 8, # nineteen = 8
-  20   => 6, # twenty = 6
-  30   => 6, # thirty = 6
-  40   => 5, # forty = 5
-  50   => 5, # fifty = 5
-  60   => 5, # sixty = 5
-  70   => 7, # seventy = 7
-  80   => 6, # eighty = 6
-  90   => 6, # ninety = 6
-  100  => 13, # one hundred and = 3+7+3 = 13
-  200  => 13, # two hundred and = 3+7+3 = 13
-  300  => 15, # three hundred and = 5+7+3 = 15
-  400  => 14, # four hundred and = 4+7+3 = 14
-  500  => 14, # five hundred and = 4+7+3 = 14
-  600  => 13, # six hundred and = 3+7+3 = 13
-  700  => 15, # seven hundred and = 5+7+3 = 15
-  800  => 15, # eight hundred and = 5+7+3 = 15
-  900  => 19, # nine hundred and  = 9+7+3 = 19
-  1000 => 14  # one thousand and = 3+8+3 = 14
+  1   => 3, # one = 3
+  2   => 3, # two = 3
+  3   => 5, # three = 5
+  4   => 4, # four = 4
+  5   => 4, # five = 4
+  6   => 3, # six = 3
+  7   => 5, # seven = 5
+  8   => 5, # eight = 5
+  9   => 4, # nine = 4
+);
+my %twoHash = (
+  10  => 3, # ten = 3
+  11  => 6, # eleven = 6
+  12  => 6, # twelve = 6
+  13  => 8, # thirteen = 8
+  14  => 8, # fourteen = 8
+  15  => 7, # fifteen = 7
+  16  => 7, # sixteen = 7
+  17  => 9, # seventeen = 9
+  18  => 8, # eighteen = 8
+  19  => 8, # nineteen = 8
+  2   => 6, # twenty = 6
+  3   => 6, # thirty = 6
+  4   => 5, # forty = 5
+  5   => 5, # fifty = 5
+  6   => 5, # sixty = 5
+  7   => 7, # seventy = 7
+  8   => 6, # eighty = 6
+  9   => 6, # ninety = 6
+);
+my %threeHash = (
+  1  => 13, # one hundred and = 3+7+3 = 13
+  2  => 13, # two hundred and = 3+7+3 = 13
+  3  => 15, # three hundred and = 5+7+3 = 15
+  4  => 14, # four hundred and = 4+7+3 = 14
+  5  => 14, # five hundred and = 4+7+3 = 14
+  6  => 13, # six hundred and = 3+7+3 = 13
+  7  => 15, # seven hundred and = 5+7+3 = 15
+  8  => 15, # eight hundred and = 5+7+3 = 15
+  9  => 19, # nine hundred and  = 9+7+3 = 19
+);
+my %fourHash = (
+  1 => 14  # one thousand and = 3+8+3 = 14
 );
 
 # generate all numbers
 my @numbers = (1..1000); # create array of all the numbers
 
-# iterate through numbers and check hash
+# create variables
+my @ones; # keep all one digit numbers
+my @twos; # keep all two digit numbers
+my @threes; # keep all three digit numbers
+
+# there is no @fours because there is only one four digit number
+
+# iterate through numbers and sort
+for (@numbers) {
+	@temp = split "", $_; # split number into array
+	$realNum = $_; # save actual number
+	if (scalar(@temp) == 1) { # check one digit numbers
+		push (@ones, $realNum); # put number with one digits
+	} elsif (scalar(@temp) == 2) { # check two digit numbers
+		push (@twos, $realNum); # put number with two digits
+	} elsif (scalar(@temp) == 3) { # check three digit numbers
+		push (@threes, $realNum); # put number with three digits
+	}
+}
 
 # check one digit numbers
-
-# save sum of all one digit numbers
+my $totalOnes;
+for (@ones) {
+	$totalOnes += $oneHash{$_};
+}
 
 # check two digit numbers
-
-# save sum of all 2 digit numbers
+my $totalTwos; # make total two digit number variable
+for (@twos) { # loop through two digit numbers
+	if ($_ < 20) { # check for numbers less than 20
+		$totalTwos += $twoHash{$_}; # add lengths to two digit var
+	} else { # all greater numbers
+		my $temp = $_; #temp store number
+		my @tempNum = split "", $temp; # temp store number as array
+		$totalTwos += 9*$twoHash{$tempNum[0]} + $totalOnes;
+	}
+}
 
 # check three digit numbers
+my $totalThrees; # make total three digit number variable
 
 # add eleven to total for 'one thousand'
 
