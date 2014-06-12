60d4201e1a283659a049584ceb01134a6cb2f55d
etleung21 <mirageknight213@gmail.com>
2014-06-12 17:46:51 -0500

    comment largestPrimeFactor

--- largestPrimeFactor.pl
+++ largestPrimeFactor.pl
@@ -5,25 +5,25 @@
 
 use strict;
 
-my $n = 600851475143;
+my $n = 600851475143; # number to factor
 
-my $t = 1;
-while ( $t == 1 ) {
-    $n = &prime($n);
+my $t = 1; # constant to keep while loop going
+while ( $t == 1 ) { 
+    $n = &prime($n); # run prime factorization subroutine
 }
 
-sub prime {
-    my ($n) = @_;
-    for ( my $i = 2; $i <= $n; $i++ ) {
-        if ( $n % $i == 0 ) {
-            if ( $n / $i == 1 ) {
-                print "$n\n";
+sub prime { # prime factorization subroutine
+    my ($n) = @_; # take in large number to factor
+    for ( my $i = 2; $i <= $n; $i++ ) { # start at 2, until get to num
+        if ( $n % $i == 0 ) { # check if large num divisible by number
+            if ( $n / $i == 1 ) { # if you divide by number factoring...
+                print "$n\n"; # print out large prime number
                 $t = 0;
             }
             else {
                 my $ans = $n / $i;
-                return $ans;
-                last;
+                return $ans; # return smaller number
+                last; # exit loop
             }
         }
     }
