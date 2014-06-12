f4bac200f45ddfd6cd466a95fc4e96a8072c76b3
etleung21 <mirageknight213@gmail.com>
2014-06-12 17:29:58 -0500

    comment evenFibo

--- evenFibo.pl
+++ evenFibo.pl
@@ -4,14 +4,14 @@
 # Consider terms in Fibonacci seq whose values do not exceed four million
 # Find the sum of the even-valued terms
 
-my $j = 2;
-my $k = 1;
-my $sum = 0;
-while ( $j < 4000000 ) {
-    if ( $j % 2 == 0 ) {
-        $sum += $j;
+my $j = 2; # create second number
+my $k = 1; # create first number
+my $sum = 0; # init sum variable
+while ( $j < 4000000 ) { # don't want it to exceed four million
+    if ( $j % 2 == 0 ) { # for even numbers...
+        $sum += $j; # ...add to the sum
     }
-    $j = $j + $k;
-    $k = $j - $k;
+    $j = $j + $k; # increase second number in sequence
+    $k = $j - $k; # subtract new 2nd num with original 1st number
 }
-print "$sum\n";
+print "$sum\n"; # print final sum
