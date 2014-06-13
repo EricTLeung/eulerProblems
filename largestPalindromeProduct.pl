26e022ede3def3ce6ee326d7a730156eaa73adc8
etleung21 <mirageknight213@gmail.com>
2014-06-12 21:35:40 -0500

    comment through largestPalindromeProduct

--- largestPalindromeProduct.pl
+++ largestPalindromeProduct.pl
@@ -3,31 +3,31 @@
 # Program 'largestPalindromeProduct'
 # Find the largest palindrom made from the product of two 3-digit numnbers
 
-my $great = 0;
-for ( my $j = 100; $j <= 999; $j++ ) {
-    for ( my $k = 100; $k <= 999; $k++ ) {
-        my $product = $j * $k;
-        if ( &pali($product) ) {
-            if ( $product > $great ) {
-                $great = $product;
+my $great = 0; # init variable for answer
+for ( my $j = 100; $j <= 999; $j++ ) { # first three digit number
+    for ( my $k = 100; $k <= 999; $k++ ) { # second three digit number
+        my $product = $j * $k; # find the product
+        if ( &pali($product) ) { # T of F for if it is palindrome
+            if ( $product > $great ) { # if I get greater palindrome...
+                $great = $product; # ...keep the number
             }
         }
     }  
 }
 
-print "$great\n";
+print "$great\n"; # give answer found
 
 
-sub pali { # Check if num is palindrom
-    my ($n) = @_;
-    my @num = split "", $n;
-    my @numRev = reverse( @num );
-    my $first = join "", @num;
-    my $sec = join "", @numRev;
-    if ( $first == $sec ) {
-        return 1;
+sub pali { # Check if num is palindrome
+    my ($n) = @_; # save number
+    my @num = split "", $n; # split number into array
+    my @numRev = reverse( @num ); # reverse the array
+    my $first = join "", @num; # change back to number
+    my $sec = join "", @numRev; # change back to number
+    if ( $first == $sec ) { # check if they are the same
+        return 1; # return true
     } 
-    else {
-        return 0;
+    else { # if not the same...
+        return 0; # ...return false
     }
 }
